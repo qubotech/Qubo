af14052cba8b40c9aa57d7bf370c3f9cefd008bb
Mr. AKS <aksnetlink@gmail.com>
2025-11-10 23:19:50 +0530

    3

--- src/config/db.js
+++
@@ -1,5 +0,0 @@
-const MONGODB_URI = import.meta.env.VITE_MONGODB_URI
-
-export const API_BASE_URL = 'https://quboo-backend.vercel.app/api' // We'll deploy backend separately
-
-export default MONGODB_URI
